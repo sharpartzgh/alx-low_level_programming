51e7b5e4983df25070f2f5e0674419b4be312101
sharpartzgh <sharpartzprojects@yahoo.com>
2023-02-16 15:08:46 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints with puts
+ *
+ * Return:0(Succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
