9e0548939548f01f71a539ee7d9dfc4adcb33b85
sharpartzgh <sharpartzprojects@yahoo.com>
2023-02-27 15:14:05 -0800

    array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n element of an array of integers
+ * Owned by Donatus
+ * @a: int to check
+ * @n: int to check
+ * Return: 0 is success
+ */
+void print_array(int *a, int n)
+{
+	int y;
+
+	for (y = 0; y < n; y++)
+		if (y != n - 1)
+		printf("%d, ", a[y]);
+		else
+			printf("%d", a[y]);
+printf("\n");
+}
